b304f8e2715df4f8257a47080ebab1d5b1cd31f2
Hugo Colin <colin.hugo.89@gmail.com>
2022-06-06 13:23:14 -0500

    Archivo que convierte las imagenes de base64 a file y las sube a su respectiva carpeta en servidor

---
+++ helpers/upload-file.js
@@ -0,0 +1,42 @@
+const path = require( 'path' );
+const fs = require( 'fs' );
+
+const { v4: uuidv4 } = require('uuid');
+
+function subirFoto( dataURI, extensionesValidas = [ 'png', 'jpg', 'jpeg', 'webp' ], carpeta ) {
+
+    return new Promise( ( resolve, reject ) => {
+
+        const extension = dataURI.split( ';' )[ 0 ].split( '/' )[ 1 ];
+
+        if ( !extensionesValidas.includes( extension ) ) {
+            return reject( `Extensión ${ extension } no válida, extensiones válidas ${ extensionesValidas }` );
+        }
+
+        const posicion = dataURI.indexOf( ',' ) + 1;
+
+        const base64Data = dataURI.slice( posicion );
+        const binaryData = new Buffer.from( base64Data, 'base64' ).toString( 'binary' );
+
+        const nombre = uuidv4() + '.' + extension;
+        const carpetaContenedora = path.join( __dirname, '../uploads/', carpeta );
+
+        if ( !fs.existsSync( carpetaContenedora ) ) {
+            fs.mkdirSync( carpetaContenedora );
+        }
+
+        const uploadPath = path.join( __dirname, '../uploads/', carpeta, nombre );
+
+        fs.writeFile( uploadPath, binaryData, 'binary', ( err ) => {
+
+            if( err ){ 
+                console.error( 'Error al mover la imagen.', err );
+                return reject( 'Error al mover la imagen.' );
+            }
+
+            resolve( nombre );
+        } );
+    } );
+};
+
+module.exports = subirFoto;
