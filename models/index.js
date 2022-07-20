f099d74c6ac15ca69b4e8bba6b861fb4198a1f87
Hugo Colin <colin.hugo.89@gmail.com>
2022-07-20 15:22:35 -0500

    Se termino el CRUD de las zonas

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const Order = require( './order.model' );
 const Product = require( './product.model' );
 const User = require( './user.model' );
+const Zone = require( './zone.model' );
 
 module.exports = {
     Order,
     Product,
-    User
+    User,
+    Zone
 }
