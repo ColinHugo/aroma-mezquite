17bf3270993d3606e1e4b0d7c6e1e1cd267039ce
Hugo Colin <colin.hugo.89@gmail.com>
2022-08-16 14:29:43 -0500

    Se quito la limitacion de inicio de sesion

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,11 @@
 const router = require( 'express' ).Router();
 const { check } = require( 'express-validator' );
 
-const { limiters, validateFields } = require( '../middlewares' );
+const { validateFields } = require( '../middlewares' );
 
 const login = require( '../controllers/auth.controller' );
 
 router.post( '/login', [
-    limiters.limiterLogin,
     check( 'correo', 'Ingrese un correo válido' ).escape().trim().isEmail(),
     check( 'password', 'La contraseña es obligatoria' ).escape().trim().notEmpty(),
     validateFields
