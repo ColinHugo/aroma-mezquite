92e6eec996a1f536f768fd2d9d234e4177ddbfb6
Hugo Colin <colin.hugo.89@gmail.com>
2022-08-16 14:58:19 -0500

    Se anadio al index colecciones

--- models/index.js
+++ models/index.js
@@ -1,13 +1,17 @@
+const Delivery = require( './delivery.model' );
 const Order = require( './order.model' );
 const Product = require( './product.model' );
+const ReservationTable = require( './reservation-table' );
 const ReservationZone = require( './reservation-zone' );
 const Table = require( './table.model' );
 const User = require( './user.model' );
 const Zone = require( './zone.model' );
 
 module.exports = {
+    Delivery,
     Order,
     Product,
+    ReservationTable,
     ReservationZone,
     Table,
     User,
